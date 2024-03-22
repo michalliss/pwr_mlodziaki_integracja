c1459d798921c304da5e61f4e5468d970907388b
michalliss <michal.j.liss@gmail.com>
2024-03-22 21:36:59 +0100

    ws

--- services/frontend/app/page.tsx
+++ services/frontend/app/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
 import Image from "next/image";
 
+import { Socket, io } from "socket.io-client"
+
+
+
 export default function Home() {
+
+  const socket = io('0.0.0.0:8000',  {
+    path: '',
+  });
+
+  socket.on('connect', () => {
+    console.log('Successfully connected!');
+  });
+
+  socket.connect();
+
+  console.log("dupa")
+
   return (
     <div>
+      {socket.connected ? <h1>Connected</h1> : <h1>Not connected</h1>}
       <h1>Next.js + TypeScript + Tailwind CSS</h1>
-      <Image src="/nextjs.svg" alt="Next.js Logo" width={200} height={100} />
     </div>
   );
 }
