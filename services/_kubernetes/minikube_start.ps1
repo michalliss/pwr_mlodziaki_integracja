bf5fffd05c88a83430cf7b3523bf78864c8f15de
Mendrejk <bastian.lakomy@gmail.com>
2024-03-22 18:44:04 +0100

    Add minikube tunnel in windows verison of minikube_start

--- services/_kubernetes/minikube_start.ps1
+++ services/_kubernetes/minikube_start.ps1
@@ -1,3 +1,16 @@
 minikube start --memory 4096 --cpus 2
+
+# Start minikube tunnel in the background
+$tunnelJob = Start-Job -ScriptBlock { minikube tunnel }
+
+# Wait for the tunnel to successfully start
+while ($true) {
+    $jobOutput = Receive-Job -Job $tunnelJob -Keep
+    if ($jobOutput -match "\* Tunnel successfully started") {
+        break
+    }
+    Start-Sleep -Seconds 1
+}
+
 minikube addons enable ingress
 minikube dashboard
