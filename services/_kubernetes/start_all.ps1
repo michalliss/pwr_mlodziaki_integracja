eaaaeb5fca502c9008895eae8ebc4a90e4ddcdfc
deamacto <pszon.joanna@gmail.com>
2024-05-04 16:07:51 +0200

    added latest version of the project

--- services/_kubernetes/start_all.ps1
+++ services/_kubernetes/start_all.ps1
@@ -1,45 +1,45 @@
-minikube kubectl -- apply -f rooms/rooms.yaml
-minikube kubectl -- apply -f rooms/rooms-service.yaml
+kubectl apply -f rooms/rooms.yaml
+kubectl apply -f rooms/rooms-service.yaml
+kubectl apply -f rooms-db/rooms-db.yaml
+kubectl apply -f rooms-db/rooms-db-service.yaml
+kubectl apply -f rooms-db/rooms-db-volume.yaml
+kubectl apply -f rooms-db/rooms-db-volume-claim.yaml
+ 
+kubectl apply -f users/users.yaml
+kubectl apply -f users/users-service.yaml
+kubectl apply -f users-db/users-db.yaml
+kubectl apply -f users-db/users-db-service.yaml
+kubectl apply -f users-db/users-db-volume.yaml
+kubectl apply -f users-db/users-db-volume-claim.yaml
+ 
+kubectl apply -f chat/chat.yaml
+kubectl apply -f chat/chat-service.yaml
+kubectl apply -f chat-db/chat-db.yaml
+kubectl apply -f chat-db/chat-db-service.yaml
+kubectl apply -f chat-db/chat-db-volume.yaml
+kubectl apply -f chat-db/chat-db-volume-claim.yaml
+ 
+kubectl apply -f videos/videos.yaml
+kubectl apply -f videos/videos-service.yaml
+kubectl apply -f videos-db/videos-db.yaml
+kubectl apply -f videos-db/videos-db-service.yaml
+kubectl apply -f videos-db/videos-db-volume.yaml
+kubectl apply -f videos-db/videos-db-volume-claim.yaml
+ 
+kubectl apply -f zookeeper/zookeeper.yaml
+kubectl apply -f zookeeper/zookeeper-service.yaml
+kubectl apply -f kafka/kafka.yaml
+kubectl apply -f kafka/kafka-service.yaml
 
-minikube kubectl -- apply -f rooms-db/rooms-db.yaml
-minikube kubectl -- apply -f rooms-db/rooms-db-service.yaml
-minikube kubectl -- apply -f rooms-db/rooms-db-volume.yaml
-minikube kubectl -- apply -f rooms-db/rooms-db-volume-claim.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
+kubectl apply -f kong/gateway.yaml
+kubectl apply -f kong/gateway_class.yaml
 
-# --
+helm repo add kong https://charts.konghq.com
+helm repo update
+helm install kong kong/ingress -n kong --create-namespace 
 
-minikube kubectl -- apply -f users/users.yaml
-minikube kubectl -- apply -f users/users-service.yaml
-
-minikube kubectl -- apply -f users-db/users-db.yaml
-minikube kubectl -- apply -f users-db/users-db-service.yaml
-minikube kubectl -- apply -f users-db/users-db-volume.yaml
-minikube kubectl -- apply -f users-db/users-db-volume-claim.yaml
-
-# --
-
-minikube kubectl -- apply -f chat/chat.yaml
-minikube kubectl -- apply -f chat/chat-service.yaml
-
-minikube kubectl -- apply -f chat-db/chat-db.yaml
-minikube kubectl -- apply -f chat-db/chat-db-service.yaml
-minikube kubectl -- apply -f chat-db/chat-db-volume.yaml
-minikube kubectl -- apply -f chat-db/chat-db-volume-claim.yaml
-
-# --
-
-minikube kubectl -- apply -f videos/videos.yaml
-minikube kubectl -- apply -f videos/videos-service.yaml
-
-minikube kubectl -- apply -f videos-db/videos-db.yaml
-minikube kubectl -- apply -f videos-db/videos-db-service.yaml
-minikube kubectl -- apply -f videos-db/videos-db-volume.yaml
-minikube kubectl -- apply -f videos-db/videos-db-volume-claim.yaml
-
-# --
-
-minikube kubectl -- apply -f zookeeper/zookeeper.yaml
-minikube kubectl -- apply -f zookeeper/zookeeper-service.yaml
-minikube kubectl -- apply -f kafka/kafka.yaml
-minikube kubectl -- apply -f kafka/kafka-service.yaml
-minikube kubectl -- apply -f ingress.yaml
+kubectl apply -f kong/videos_route.yaml
+kubectl apply -f kong/chat_route.yaml
+kubectl apply -f kong/rooms_route.yaml
+kubectl apply -f kong/users_route.yaml
--- services/_kubernetes/stop_all.ps1
+++ services/_kubernetes/stop_all.ps1
@@ -1,33 +1,40 @@
-minikube kubectl -- delete -f rooms/rooms.yaml
-minikube kubectl -- delete -f rooms/rooms-service.yaml
-minikube kubectl -- delete -f rooms-db/rooms-db.yaml
-minikube kubectl -- delete -f rooms-db/rooms-db-service.yaml
-minikube kubectl -- delete -f rooms-db/rooms-db-volume.yaml
-minikube kubectl -- delete -f rooms-db/rooms-db-volume-claim.yaml
+kubectl delete -f rooms/rooms.yaml
+kubectl delete -f rooms/rooms-service.yaml
+kubectl delete -f rooms-db/rooms-db.yaml
+kubectl delete -f rooms-db/rooms-db-service.yaml
+kubectl delete -f rooms-db/rooms-db-volume.yaml
+kubectl delete -f rooms-db/rooms-db-volume-claim.yaml
 
-minikube kubectl -- delete -f users/users.yaml
-minikube kubectl -- delete -f users/users-service.yaml
-minikube kubectl -- delete -f users-db/users-db.yaml
-minikube kubectl -- delete -f users-db/users-db-service.yaml
-minikube kubectl -- delete -f users-db/users-db-volume.yaml
-minikube kubectl -- delete -f users-db/users-db-volume-claim.yaml
+kubectl delete -f users/users.yaml
+kubectl delete -f users/users-service.yaml
+kubectl delete -f users-db/users-db.yaml
+kubectl delete -f users-db/users-db-service.yaml
+kubectl delete -f users-db/users-db-volume.yaml
+kubectl delete -f users-db/users-db-volume-claim.yaml
 
-minikube kubectl -- delete -f chat/chat.yaml
-minikube kubectl -- delete -f chat/chat-service.yaml
-minikube kubectl -- delete -f chat-db/chat-db.yaml
-minikube kubectl -- delete -f chat-db/chat-db-service.yaml
-minikube kubectl -- delete -f chat-db/chat-db-volume.yaml
-minikube kubectl -- delete -f chat-db/chat-db-volume-claim.yaml
+kubectl delete -f chat/chat.yaml
+kubectl delete -f chat/chat-service.yaml
+kubectl delete -f chat-db/chat-db.yaml
+kubectl delete -f chat-db/chat-db-service.yaml
+kubectl delete -f chat-db/chat-db-volume.yaml
+kubectl delete -f chat-db/chat-db-volume-claim.yaml
 
-minikube kubectl -- delete -f videos/videos.yaml
-minikube kubectl -- delete -f videos/videos-service.yaml
-minikube kubectl -- delete -f videos-db/videos-db.yaml
-minikube kubectl -- delete -f videos-db/videos-db-service.yaml
-minikube kubectl -- delete -f videos-db/videos-db-volume.yaml
-minikube kubectl -- delete -f videos-db/videos-db-volume-claim.yaml
+kubectl delete -f videos/videos.yaml
+kubectl delete -f videos/videos-service.yaml
+kubectl delete -f videos-db/videos-db.yaml
+kubectl delete -f videos-db/videos-db-service.yaml
+kubectl delete -f videos-db/videos-db-volume.yaml
+kubectl delete -f videos-db/videos-db-volume-claim.yaml
 
-minikube kubectl -- delete -f zookeeper/zookeeper.yaml
-minikube kubectl -- delete -f zookeeper/zookeeper-service.yaml
-minikube kubectl -- delete -f kafka/kafka.yaml
-minikube kubectl -- delete -f kafka/kafka-service.yaml
-minikube kubectl -- delete -f ingress.yaml
+kubectl delete -f zookeeper/zookeeper.yaml
+kubectl delete -f zookeeper/zookeeper-service.yaml
+kubectl delete -f kafka/kafka.yaml
+kubectl delete -f kafka/kafka-service.yaml
+
+kubectl delete -f kong/gateway.yaml
+kubectl delete -f kong/gateway_class.yaml
+
+kubectl delete -f kong/videos_route.yaml
+kubectl delete -f kong/chat_route.yaml
+kubectl delete -f kong/rooms_route.yaml
+kubectl delete -f kong/users_route.yaml
